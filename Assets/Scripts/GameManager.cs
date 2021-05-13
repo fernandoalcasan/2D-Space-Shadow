476e8404be70763b83621994b7b294e0e974c301
fernandoalcasan <fernandoalcasan@outlook.com>
2021-05-13 16:14:00 -0500

    Main menu created

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.R) && _gameOver)
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1);
         }
     }
 
